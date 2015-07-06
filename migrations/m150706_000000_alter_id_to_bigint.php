cf19d7552ed465bde0ee83e512681d3e83c89162
darren.ng <darren.ng@dpodium.com>
2015-07-06 18:58:22 +0800

    Convert id to bigint

---
+++ migrations/m150706_000000_alter_id_to_bigint.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Expression;
+
+/**
+ * Migration to create or remove audit trail entry table
+ * 
+ * @author Darren Ng, Dynamic Podium
+ * @link http://www.dpodium.com
+ * @license MIT
+ */
+class m150706_000000_alter_id_to_bigint extends \yii\db\Migration {
+
+    /**
+     * @inheritdoc
+     */
+    public function up() {
+        $this->alterColumn('{{%audit_trail_entry}}', 'id', Schema::TYPE_BIGINT . " auto_increment");
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down() {
+        $this->alterColumn('{{%audit_trail_entry}}', 'id', Schema::TYPE_INT . " auto_increment");
+    }
+
+}
